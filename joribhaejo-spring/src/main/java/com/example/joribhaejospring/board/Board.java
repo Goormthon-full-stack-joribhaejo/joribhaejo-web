8e126ccc7fe6d1e2634a3bdbfce6db9dba10fce3
KTH <pni2396@gmail.com>
2025-08-05 15:22:53 +0900

    feat: entity, schema 정의

---
+++ joribhaejo-spring/src/main/java/com/example/joribhaejospring/board/Board.java
@@ -0,0 +1,30 @@
+package com.example.joribhaejospring.board;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "boards")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private LocalDateTime updatedAt;
+}

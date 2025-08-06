a907ee92a7b536a5b505f11d4bdae232bdf1b26a
KTH <pni2396@gmail.com>
2025-08-06 14:32:44 +0900

    fix: Security 수정

--- joribhaejo-spring/src/main/java/com/example/joribhaejospring/common/SecurityConfig.java
+++ joribhaejo-spring/src/main/java/com/example/joribhaejospring/common/SecurityConfig.java
@@ -1,17 +1,19 @@
 package com.example.joribhaejospring.common;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,7 +21,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     // 모든 요청 허용
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,13 +34,15 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers(HttpMethod.POST, "/api/users/login", "/api/users/signup").permitAll()
+                                .requestMatchers("/api/users/login", "/api/users/signup").permitAll()
                                 .requestMatchers("/error", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/posts/*", "/api/posts", "/api/posts/*/comments").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/likes", "/api/boards").permitAll()
+                                .requestMatchers("/api/posts/*", "/api/posts", "/api/posts/*/comments").permitAll()
+                                .requestMatchers("/api/likes", "/api/boards").permitAll()
                                 .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+
                         //.anyRequest().permitAll() // 모든 요청 허용
                 )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // JWT 필터 추가
                 .formLogin(AbstractHttpConfigurer::disable)   // 기본 폼 로그인 비활성화
                 .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
@@ -43,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOriginPattern("http://jorib.mobidic.shop"); // 모든 Origin 허용 (개발용)
+        config.addAllowedOriginPattern("*"); // 모든 Origin 허용 (개발용)
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.addAllowedHeader("*");        // 모든 헤더 허용
         config.setAllowCredentials(true);    // 인증 정보 포함 허용 (쿠키 등)

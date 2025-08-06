d2ecb4a46bf02d4079b0ca7ed4c2d7def8d4256e
KTH <pni2396@gmail.com>
2025-08-06 13:16:39 +0900

    fix: Security config 수정
    - 비회원 기능 외에 모두 Authentication 명시

--- joribhaejo-spring/src/main/java/com/example/joribhaejospring/user/UserService.java
+++ joribhaejo-spring/src/main/java/com/example/joribhaejospring/user/UserService.java
@@ -5,6 +5,7 @@ import com.example.joribhaejospring.common.exception.DuplicateRequestException;
 import com.example.joribhaejospring.user.dto.LoginRequest;
 import com.example.joribhaejospring.user.dto.LoginResponse;
 import com.example.joribhaejospring.user.dto.SignupRequest;
+import com.example.joribhaejospring.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
@@ -50,5 +51,11 @@ public class UserService {
                 .id(user.getId())
                 .accessToken(jwtUtil.generateAccessToken(auth))
                 .build();
+    }
+
+    public UserResponse getMyInfo() {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return new UserResponse(currentUser.getId(), currentUser.getUsername(), currentUser.getEmail());
     }
 }

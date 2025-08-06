18825bd05640b0d604939b17f3393cb61c3ad149
KTH <pni2396@gmail.com>
2025-08-06 11:07:46 +0900

    feat: Controller 점검 및 수정

--- joribhaejo-spring/src/main/java/com/example/joribhaejospring/message/MessageController.java
+++ joribhaejo-spring/src/main/java/com/example/joribhaejospring/message/MessageController.java
@@ -1,6 +1,7 @@
 package com.example.joribhaejospring.message;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,32 +14,33 @@ public class MessageController {
 
     // 받은 쪽지 목록
     @GetMapping("/inbox")
-    public List<MessageDto> getInboxMessages() {
-        return messageService.getInboxMessages();
+    public ResponseEntity<List<MessageDto>> getInboxMessages() {
+        return ResponseEntity.ok(messageService.getInboxMessages());
     }
 
     // 보낸 쪽지 목록
     @GetMapping("/sent")
-    public List<MessageDto> getSentMessages() {
-        return messageService.getSentMessages();
+    public ResponseEntity<List<MessageDto>> getSentMessages() {
+        return ResponseEntity.ok(messageService.getSentMessages());
     }
 
     // 쪽지 상세 조회
     @GetMapping("/{messageId}")
-    public MessageDto getMessageDetail(@PathVariable Integer messageId) {
-        return messageService.getMessageDetail(messageId);
+    public ResponseEntity<MessageDto> getMessageDetail(@PathVariable Integer messageId) {
+        return ResponseEntity.ok(messageService.getMessageDetail(messageId));
     }
 
     // 쪽지 보내기
     @PostMapping("/{receiverId}")
-    public MessageDto sendMessage(@PathVariable Integer receiverId,
+    public ResponseEntity<MessageDto> sendMessage(@PathVariable Integer receiverId,
                                   @RequestBody MessageRequest request) {
-        return messageService.sendMessage(receiverId, request.getContent());
+        return ResponseEntity.ok(messageService.sendMessage(receiverId, request.getContent()));
     }
 
     // 쪽지 삭제 (본인만)
     @DeleteMapping("/{messageId}")
-    public void deleteMessage(@PathVariable Integer messageId) {
+    public ResponseEntity<Void> deleteMessage(@PathVariable Integer messageId) {
         messageService.deleteMessage(messageId);
+        return ResponseEntity.ok().build();
     }
 }

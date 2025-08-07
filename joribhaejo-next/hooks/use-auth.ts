ac43df0204afeb95f9b6a22c11efd0524b34743a
KTH <pni2396@gmail.com>
2025-08-08 07:03:01 +0900

    fix: 인증 정보 UI 반영 시간 수정

--- joribhaejo-next/hooks/use-auth.ts
+++ joribhaejo-next/hooks/use-auth.ts
@@ -8,8 +8,8 @@ export function useCurrentUser() {
   return useQuery<User, Error>({
     queryKey: ['currentUser'],
     queryFn: () => userApi.getCurrentUser(),
-    staleTime: 5 * 60 * 1000, // 5분
-    gcTime: 10 * 60 * 1000, // 10분
+    staleTime: 0, // 5분
+    gcTime: 0, // 10분
     enabled: !!accessToken, // accessToken이 있을 때만 쿼리 실행
   })
 }
@@ -20,8 +20,8 @@ export function useLikedPostIds() {
   return useQuery<number[], Error>({
     queryKey: ['likedPostIds'],
     queryFn: () => userApi.getLikedPostIds(),
-    staleTime: 5 * 60 * 1000, // 5분
-    gcTime: 10 * 60 * 1000, // 10분
+    staleTime: 0, // 5분
+    gcTime: 0, // 10분
     enabled: !!accessToken, // accessToken이 있을 때만 쿼리 실행
   })
 }
